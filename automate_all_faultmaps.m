9beb06dcc4010c949e77e0dc00d5e1d540e481c7
Mark Gottscho <mgottscho@ucla.edu>
2014-05-17 16:48:00 -0700

    added v7.3 flag to MATLAB .mat saving of faultmaps. This is super large files can be saved. This will be OK because v7.3 is at least 6 years old.

--- automate_all_faultmaps.m
+++ automate_all_faultmaps.m
@@ -15,7 +15,7 @@ mkdir faultmaps
 % Compute L1-A fault maps
 display('L1-A fault maps...');
 [block_faultmaps_L1_A, vdd_mins_L1_A, vdd_mins_nonfaulty_L1_A] = generate_fault_maps('parameters/gem5params-L1-A.csv', 64*2^10*8, 4, 512, [start_index:end_index], 0, 'faultmaps', 'L1', 'A');
-save('faultmaps/faultmaps_L1_A.mat');
+save('faultmaps/faultmaps_L1_A.mat', '-v7.3');
 clear block_faultmaps_L1_A;
 clear vdd_mins_L1_A;
 clear vdd_mins_nonfaulty_L1_A;
@@ -23,7 +23,7 @@ clear vdd_mins_nonfaulty_L1_A;
 % Compute L2-A fault maps
 display('L2-A fault maps...');
 [block_faultmaps_L2_A, vdd_mins_L2_A, vdd_mins_nonfaulty_L2_A] = generate_fault_maps('parameters/gem5params-L2-A.csv', 2*2^20*8, 8, 512, [start_index:end_index], 0, 'faultmaps', 'L2', 'A');
-save('faultmaps/faultmaps_L2_A.mat');
+save('faultmaps/faultmaps_L2_A.mat', '-v7.3');
 clear block_faultmaps_L2_A;
 clear vdd_mins_L2_A;
 clear vdd_mins_nonfaulty_L2_A;
@@ -31,7 +31,7 @@ clear vdd_mins_nonfaulty_L2_A;
 % Compute L1-B fault maps
 display('L1-B fault maps...');
 [block_faultmaps_L1_B, vdd_mins_L1_B, vdd_mins_nonfaulty_L1_B] = generate_fault_maps('parameters/gem5params-L1-B.csv', 256*2^10*8, 8, 512, [start_index:end_index], 0, 'faultmaps', 'L1', 'B');
- save('faultmaps/faultmaps_L1_B.mat');
+ save('faultmaps/faultmaps_L1_B.mat', '-v7.3');
 clear block_faultmaps_L1_B;
 clear vdd_mins_L1_B;
 clear vdd_mins_nonfaulty_L1_B;
@@ -39,7 +39,7 @@ clear vdd_mins_nonfaulty_L1_B;
 % Compute L2-B fault maps
 display('L2-B fault maps...');
 [block_faultmaps_L2_B, vdd_mins_L2_B, vdd_mins_nonfaulty_L2_B] = generate_fault_maps('parameters/gem5params-L2-B.csv', 8*2^20*8, 16, 512, [start_index:end_index], 0, 'faultmaps', 'L2', 'B');
-save('faultmaps/faultmaps_L2_B.mat');
+save('faultmaps/faultmaps_L2_B.mat', '-v7.3');
 clear block_faultmaps_L2_B;
 clear vdd_mins_L2_B;
 clear vdd_mins_nonfaulty_L2_B;
